7213a947addfb289d5e822fea20dd3e52d0b770a
Max Gotts <spamgotts@gmail.com>
2019-06-03 15:25:51 -0700

    typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var SpotifyWebApi = require('spotify-web-api-node');
 
 var sapi = new SpotifyWebApi(); // The Spotify API ==> sapi
 
-spotifyApi.getUser('petteralexis') // getMe() ;; getPlaylist('5ieJqeLJjjI8iJWaxeBLuK') ;; 
+sapi.getUser('petteralexis') // getMe() ;; getPlaylist('5ieJqeLJjjI8iJWaxeBLuK') ;; 
 	.then(data => {
 		console.log(data.body)
 	}, err => { console.error(err); })
