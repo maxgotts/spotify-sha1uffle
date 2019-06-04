3813fe01c240e0ef11d01c33870219f3ef881277
Max Gotts <spamgotts@gmail.com>
2019-06-03 21:38:25 -0700

    -

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var SpotifyWebApi = require('spotify-web-api-node');
 
 var sapi = new SpotifyWebApi(); // The Spotify API ==> sapi
 
-sapi.getUser('petteralexis') // getMe() ;; getPlaylist('5ieJqeLJjjI8iJWaxeBLuK') ;; 
+sapi.getPlaylist('5ieJqeLJjjI8iJWaxeBLuK') // getUser('petteralexis') // getMe() ;; getPlaylist('5ieJqeLJjjI8iJWaxeBLuK') ;; 
 	.then(data => {
 		console.log(data.body)
 	}, err => { console.error(err); })
